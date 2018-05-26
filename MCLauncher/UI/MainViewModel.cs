0434b757215af951564eadc7f6fc329e13d50516
Aidar Yakupov <a-ghostik@yandex.ru>
2018-05-26 21:28:24 +0500

    add UI

---
+++ MCLauncher/UI/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+
+namespace MCLauncher.UI
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public MainViewModel()
+        {
+
+        }
+
+        public string CurrentProfile { get; set; }
+        public ObservableCollection<string> Profiles { get; set; } = new ObservableCollection<string>();
+
+        public RelayCommand Start { get; set; }
+        public RelayCommand NewProfile { get; set; }
+        public RelayCommand EditProfile { get; set; }
+        public RelayCommand DeleteProfile { get; set; }
+    }
+}
