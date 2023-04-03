294f69f9fe1946d1d46cf7e057aadde614fea8d7
AGhostik <yakupov.aidar.ru@yandex.ru>
2023-04-03 22:42:44 +0500

    reset MainContent when switch profile

--- MinecraftLauncher/Main/MainWindowViewModel.cs
+++ MinecraftLauncher/Main/MainWindowViewModel.cs
@@ -79,6 +79,7 @@ public sealed class MainWindowViewModel : ReactiveObject
             UpdateCanEditProfile();
             UpdateCanDeleteProfile();
             UpdateCanStartGame();
+            SetDefaultMainContent();
         }
     }
 
