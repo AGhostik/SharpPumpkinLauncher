8b611c453f6188c57bfa7d05c44fa4547e3f9759
AGhostik <yakupov.aidar.ru@yandex.ru>
2023-04-04 18:30:47 +0500

    add UnitTests add ProfileNameValidationTests.cs add PlayerNameValidationTests.cs

---
+++ Tests/MinecraftLauncherUnitTests/PlayerNameValidationTests.cs
@@ -0,0 +1,276 @@
+using MinecraftLauncher.Main.Validation;
+
+namespace MinecraftLauncherUnitTests;
+
+public class PlayerNameValidationTests
+{
+    [Test]
+    public void PlayerNameValidation_True_NameValid()
+    {
+        const string shortPlayerName = "AaAaAaAaAaAaAaAa";
+        var result = PlayerNameValidation.IsPlayerNameValid(shortPlayerName);
+        Assert.IsTrue(result);
+    }
+    
+    [Test]
+    public void PlayerNameValidation_True_NameValid_ContainUnderscore()
+    {
+        const string shortPlayerName = "_AaAa_";
+        var result = PlayerNameValidation.IsPlayerNameValid(shortPlayerName);
+        Assert.IsTrue(result);
+    }
+
+    [Test]
+    public void PlayerNameValidation_False_NameTooShort()
+    {
+        const string shortPlayerName = "Aa";
+        var result = PlayerNameValidation.IsPlayerNameValid(shortPlayerName);
+        Assert.IsFalse(result);
+    }
+    
+    [Test]
+    public void PlayerNameValidation_False_NameTooLong()
+    {
+        const string shortPlayerName = "AaAaAaAaAaAaAaAaA";
+        var result = PlayerNameValidation.IsPlayerNameValid(shortPlayerName);
+        Assert.IsFalse(result);
+    }
+    
+    [Test]
+    public void PlayerNameValidation_False_NameIsEmptyString()
+    {
+        var result = PlayerNameValidation.IsPlayerNameValid(string.Empty);
+        Assert.IsFalse(result);
+    }
+    
+    [Test]
+    public void PlayerNameValidation_False_NameIsNull()
+    {
+        var result = PlayerNameValidation.IsPlayerNameValid(null);
+        Assert.IsFalse(result);
+    }
+    
+    [Test]
+    public void PlayerNameValidation_False_NameContain_ExcamlationMark()
+    {
+        const string shortPlayerName = "Aaa!";
+        var result = PlayerNameValidation.IsPlayerNameValid(shortPlayerName);
+        Assert.IsFalse(result);
+    }
+    
+    [Test]
+    public void PlayerNameValidation_False_NameContain_AtSign()
+    {
+        const string shortPlayerName = "Aaa@";
+        var result = PlayerNameValidation.IsPlayerNameValid(shortPlayerName);
+        Assert.IsFalse(result);
+    }
+    
+    [Test]
+    public void PlayerNameValidation_False_NameContain_Actue()
+    {
+        const string shortPlayerName = "Aaa\'";
+        var result = PlayerNameValidation.IsPlayerNameValid(shortPlayerName);
+        Assert.IsFalse(result);
+    }
+    
+    [Test]
+    public void PlayerNameValidation_False_NameContain_SpeechMark()
+    {
+        const string shortPlayerName = "Aaa\"";
+        var result = PlayerNameValidation.IsPlayerNameValid(shortPlayerName);
+        Assert.IsFalse(result);
+    }
+    
+    [Test]
+    public void PlayerNameValidation_False_NameContain_Hash()
+    {
+        const string shortPlayerName = "Aaa#";
+        var result = PlayerNameValidation.IsPlayerNameValid(shortPlayerName);
+        Assert.IsFalse(result);
+    }
+    
+    [Test]
+    public void PlayerNameValidation_False_NameContain_NumeroSign()
+    {
+        const string shortPlayerName = "Aaa№";
+        var result = PlayerNameValidation.IsPlayerNameValid(shortPlayerName);
+        Assert.IsFalse(result);
+    }
+    
+    [Test]
+    public void PlayerNameValidation_False_NameContain_DollarSign()
+    {
+        const string shortPlayerName = "Aaa$";
+        var result = PlayerNameValidation.IsPlayerNameValid(shortPlayerName);
+        Assert.IsFalse(result);
+    }
+
+    [Test]
+    public void PlayerNameValidation_False_NameContain_SemiColon()
+    {
+        const string shortPlayerName = "Aaa;";
+        var result = PlayerNameValidation.IsPlayerNameValid(shortPlayerName);
+        Assert.IsFalse(result);
+    }
+    
+    [Test]
+    public void PlayerNameValidation_False_NameContain_PercentageSign()
+    {
+        const string shortPlayerName = "Aaa%";
+        var result = PlayerNameValidation.IsPlayerNameValid(shortPlayerName);
+        Assert.IsFalse(result);
+    }
+    
+    [Test]
+    public void PlayerNameValidation_False_NameContain_Caret()
+    {
+        const string shortPlayerName = "Aaa^";
+        var result = PlayerNameValidation.IsPlayerNameValid(shortPlayerName);
+        Assert.IsFalse(result);
+    }
+    
+    [Test]
+    public void PlayerNameValidation_False_NameContain_Colon()
+    {
+        const string shortPlayerName = "Aaa:";
+        var result = PlayerNameValidation.IsPlayerNameValid(shortPlayerName);
+        Assert.IsFalse(result);
+    }
+    
+    [Test]
+    public void PlayerNameValidation_False_NameContain_AndSign()
+    {
+        const string shortPlayerName = "Aaa&";
+        var result = PlayerNameValidation.IsPlayerNameValid(shortPlayerName);
+        Assert.IsFalse(result);
+    }
+    
+    [Test]
+    public void PlayerNameValidation_False_NameContain_QuestionMark()
+    {
+        const string shortPlayerName = "Aaa?";
+        var result = PlayerNameValidation.IsPlayerNameValid(shortPlayerName);
+        Assert.IsFalse(result);
+    }
+    
+    [Test]
+    public void PlayerNameValidation_False_NameContain_Asterix()
+    {
+        const string shortPlayerName = "Aaa*";
+        var result = PlayerNameValidation.IsPlayerNameValid(shortPlayerName);
+        Assert.IsFalse(result);
+    }
+    
+    [Test]
+    public void PlayerNameValidation_False_NameContain_OpenBracket()
+    {
+        const string shortPlayerName = "Aaa(";
+        var result = PlayerNameValidation.IsPlayerNameValid(shortPlayerName);
+        Assert.IsFalse(result);
+    }
+    
+    [Test]
+    public void PlayerNameValidation_False_NameContain_CloseBracket()
+    {
+        const string shortPlayerName = "Aaa)";
+        var result = PlayerNameValidation.IsPlayerNameValid(shortPlayerName);
+        Assert.IsFalse(result);
+    }
+    
+    [Test]
+    public void PlayerNameValidation_False_NameContain_Hyphen()
+    {
+        const string shortPlayerName = "Aaa-";
+        var result = PlayerNameValidation.IsPlayerNameValid(shortPlayerName);
+        Assert.IsFalse(result);
+    }
+    
+    [Test]
+    public void PlayerNameValidation_False_NameContain_Plus()
+    {
+        const string shortPlayerName = "Aaa+";
+        var result = PlayerNameValidation.IsPlayerNameValid(shortPlayerName);
+        Assert.IsFalse(result);
+    }
+    
+    [Test]
+    public void PlayerNameValidation_False_NameContain_EqualSign()
+    {
+        const string shortPlayerName = "Aaa=";
+        var result = PlayerNameValidation.IsPlayerNameValid(shortPlayerName);
+        Assert.IsFalse(result);
+    }
+    
+    [Test]
+    public void PlayerNameValidation_False_NameContain_Pipe()
+    {
+        const string shortPlayerName = "Aaa|";
+        var result = PlayerNameValidation.IsPlayerNameValid(shortPlayerName);
+        Assert.IsFalse(result);
+    }
+    
+    [Test]
+    public void PlayerNameValidation_False_NameContain_BackSlash()
+    {
+        const string shortPlayerName = "Aaa\\";
+        var result = PlayerNameValidation.IsPlayerNameValid(shortPlayerName);
+        Assert.IsFalse(result);
+    }
+    
+    [Test]
+    public void PlayerNameValidation_False_NameContain_ForwardSlash()
+    {
+        const string shortPlayerName = "Aaa/";
+        var result = PlayerNameValidation.IsPlayerNameValid(shortPlayerName);
+        Assert.IsFalse(result);
+    }
+    
+    [Test]
+    public void PlayerNameValidation_False_NameContain_Apostrophe()
+    {
+        const string shortPlayerName = "Aaa`";
+        var result = PlayerNameValidation.IsPlayerNameValid(shortPlayerName);
+        Assert.IsFalse(result);
+    }
+    
+    [Test]
+    public void PlayerNameValidation_False_NameContain_Tide()
+    {
+        const string shortPlayerName = "Aaa~";
+        var result = PlayerNameValidation.IsPlayerNameValid(shortPlayerName);
+        Assert.IsFalse(result);
+    }
+    
+    [Test]
+    public void PlayerNameValidation_False_NameContain_LessThan()
+    {
+        const string shortPlayerName = "Aaa<";
+        var result = PlayerNameValidation.IsPlayerNameValid(shortPlayerName);
+        Assert.IsFalse(result);
+    }
+    
+    [Test]
+    public void PlayerNameValidation_False_NameContain_GreaterThan()
+    {
+        const string shortPlayerName = "Aaa>";
+        var result = PlayerNameValidation.IsPlayerNameValid(shortPlayerName);
+        Assert.IsFalse(result);
+    }
+    
+    [Test]
+    public void PlayerNameValidation_False_NameContain_FullStop()
+    {
+        const string shortPlayerName = "Aaa.";
+        var result = PlayerNameValidation.IsPlayerNameValid(shortPlayerName);
+        Assert.IsFalse(result);
+    }
+    
+    [Test]
+    public void PlayerNameValidation_False_NameContain_Comma()
+    {
+        const string shortPlayerName = "Aaa,";
+        var result = PlayerNameValidation.IsPlayerNameValid(shortPlayerName);
+        Assert.IsFalse(result);
+    }
+}
---
+++ Tests/MinecraftLauncherUnitTests/ProfileNameValidationTests.cs
@@ -0,0 +1,34 @@
+﻿using MinecraftLauncher.Main.Validation;
+
+namespace MinecraftLauncherUnitTests;
+
+public class ProfileNameValidationTests
+{
+    [Test]
+    public void ProfileNameValidation_True_NameValid()
+    {
+        var result = ProfileNameValidation.IsProfileNameValid("Name", null);
+        Assert.IsTrue(result);
+    }
+    
+    [Test]
+    public void ProfileNameValidation_False_NameIsEmptyString()
+    {
+        var result = ProfileNameValidation.IsProfileNameValid(string.Empty, null);
+        Assert.IsFalse(result);
+    }
+    
+    [Test]
+    public void ProfileNameValidation_False_NameIsNull()
+    {
+        var result = ProfileNameValidation.IsProfileNameValid(null, null);
+        Assert.IsFalse(result);
+    }
+    
+    [Test]
+    public void ProfileNameValidation_False_NameIsRestricted()
+    {
+        var result = ProfileNameValidation.IsProfileNameValid("Name", new List<string?>() { "Name" });
+        Assert.IsFalse(result);
+    }
+}
