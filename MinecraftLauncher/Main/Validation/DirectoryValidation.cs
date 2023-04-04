d658ed81c13ca085cac217b33cac1b5c5bb0a4cd
AGhostik <yakupov.aidar.ru@yandex.ru>
2023-04-04 20:44:17 +0500

    add DirectoryValidationTests.cs improve DirectoryValidation

--- MinecraftLauncher/Main/Validation/DirectoryValidation.cs
+++ MinecraftLauncher/Main/Validation/DirectoryValidation.cs
@@ -1,3 +1,6 @@
+using System;
+using System.IO;
+using System.Linq;
 using MinecraftLauncher.Resources;
 
 namespace MinecraftLauncher.Main.Validation;
@@ -15,6 +18,40 @@ public static class DirectoryValidation
         {
             errorKey = Localization.ValidationEmpty;
             return false;
+        }
+
+        var invalidPathChars = Path.GetInvalidPathChars();
+        var previousPreviousChar = '\0';
+        var previousChar = '\0';
+        for (var i = 0; i < path.Length; i++)
+        {
+            var c = path[i];
+
+            if (i == 2 && c == '\\' && previousChar == ':' && char.IsLetter(previousPreviousChar))
+            {
+                //is absolute path
+                var drivers = Directory.GetLogicalDrives();
+                if (!drivers.Contains($"{previousPreviousChar}:\\"))
+                {
+                    errorKey = "Drive not exist";
+                    return false;
+                }
+            }
+
+            if (previousChar is '\\' or '/' and not '\0' && previousPreviousChar is '\\' or '/' and not '\0')
+            {
+                errorKey = "Empte directory in path";
+                return false;
+            }
+
+            if (invalidPathChars.Contains(c) || c is '?' or '<' or '>' or '\"' or '*' or '.' || (i != 1 && c == ':'))
+            {
+                errorKey = "Invalid character";
+                return false;
+            }
+
+            previousPreviousChar = previousChar;
+            previousChar = c;
         }
         
         errorKey = string.Empty;
---
+++ Tests/MinecraftLauncherUnitTests/DirectoryValidationTests.cs
@@ -0,0 +1,146 @@
+﻿using MinecraftLauncher.Main.Validation;
+
+namespace MinecraftLauncherUnitTests;
+
+public class DirectoryValidationTests
+{
+    [Test]
+    public void DirectoryValidation_True_RelativePath()
+    {
+        var result = DirectoryValidation.IsDirectoryValid("Minecraft");
+        Assert.IsTrue(result);
+    }
+    
+    [Test]
+    public void DirectoryValidation_True_RelativePath_ContainBackSlashes()
+    {
+        var result = DirectoryValidation.IsDirectoryValid("\\Minecraft\\");
+        Assert.IsTrue(result);
+    }
+    
+    [Test]
+    public void DirectoryValidation_True_AbsolutePath()
+    {
+        var result = DirectoryValidation.IsDirectoryValid("C:\\SomeFolder\\Minecraft");
+        Assert.IsTrue(result);
+    }
+    
+    [Test]
+    public void DirectoryValidation_True_AbsolutePath_ContainWhitespaces()
+    {
+        var result = DirectoryValidation.IsDirectoryValid("C:\\Some Folder\\Mine craft");
+        Assert.IsTrue(result);
+    }
+    
+    [Test]
+    public void DirectoryValidation_False_PathIsEmptyString()
+    {
+        var result = DirectoryValidation.IsDirectoryValid(string.Empty);
+        Assert.IsFalse(result);
+    }
+    
+    [Test]
+    public void DirectoryValidation_False_PathIsNull()
+    {
+        var result = DirectoryValidation.IsDirectoryValid(null);
+        Assert.IsFalse(result);
+    }
+    
+    [Test]
+    public void DirectoryValidation_False_IsFileRelativePath()
+    {
+        var result = DirectoryValidation.IsDirectoryValid("Minecraft\\Text.txt");
+        Assert.IsFalse(result);
+    }
+    
+    [Test]
+    public void DirectoryValidation_False_IsFileAbsolutePath()
+    {
+        var result = DirectoryValidation.IsDirectoryValid("C:\\SomeFolder\\Minecraft\\Text.txt");
+        Assert.IsFalse(result);
+    }
+
+    [Test]
+    public void DirectoryValidation_False_AbsolutePath_MissingDriveLetter()
+    {
+        var result = DirectoryValidation.IsDirectoryValid(":\\SomeFolder\\Minecraft");
+        Assert.IsFalse(result);
+    }
+    
+    [Test]
+    public void DirectoryValidation_False_AbsolutePath_DriveNotExist()
+    {
+        var result = DirectoryValidation.IsDirectoryValid("X:\\SomeFolder\\Minecraft");
+        Assert.IsFalse(result);
+    }
+    
+    [Test]
+    public void DirectoryValidation_False_PathContain_Pipe()
+    {
+        var result = DirectoryValidation.IsDirectoryValid("\\Minecraft|");
+        Assert.IsFalse(result);
+    }
+    
+    [Test]
+    public void DirectoryValidation_False_PathContain_GreaterThan()
+    {
+        var result = DirectoryValidation.IsDirectoryValid("\\Minecraft>");
+        Assert.IsFalse(result);
+    }
+    
+    [Test]
+    public void DirectoryValidation_False_PathContain_LessThan()
+    {
+        var result = DirectoryValidation.IsDirectoryValid("\\Minecraft<");
+        Assert.IsFalse(result);
+    }
+    
+    [Test]
+    public void DirectoryValidation_False_PathContain_SpeechMark()
+    {
+        var result = DirectoryValidation.IsDirectoryValid("\\Minecraft\"");
+        Assert.IsFalse(result);
+    }
+    
+    [Test]
+    public void DirectoryValidation_False_PathContain_QuestionMark()
+    {
+        var result = DirectoryValidation.IsDirectoryValid("\\Minecraft?");
+        Assert.IsFalse(result);
+    }
+    
+    [Test]
+    public void DirectoryValidation_False_PathContain_Asterix()
+    {
+        var result = DirectoryValidation.IsDirectoryValid("\\Minecraft*");
+        Assert.IsFalse(result);
+    }
+    
+    [Test]
+    public void DirectoryValidation_False_PathContain_Colon()
+    {
+        var result = DirectoryValidation.IsDirectoryValid("\\Minecraft:");
+        Assert.IsFalse(result);
+    }
+    
+    [Test]
+    public void DirectoryValidation_False_PathContain_BackSlashAfterBackSlash()
+    {
+        var result = DirectoryValidation.IsDirectoryValid("\\\\Minecraft");
+        Assert.IsFalse(result);
+    }
+    
+    [Test]
+    public void DirectoryValidation_False_PathContain_ForwardSlashAfterBackSlash()
+    {
+        var result = DirectoryValidation.IsDirectoryValid("\\/Minecraft");
+        Assert.IsFalse(result);
+    }
+    
+    [Test]
+    public void DirectoryValidation_False_PathContain_BackSlashAfterForwardSlash()
+    {
+        var result = DirectoryValidation.IsDirectoryValid("/\\Minecraft");
+        Assert.IsFalse(result);
+    }
+}
