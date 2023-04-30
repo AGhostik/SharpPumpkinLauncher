6b0495ff1555e87d98cf5b3c135b2cda8120ec90
AGhostik <yakupov.aidar.ru@yandex.ru>
2023-04-30 23:52:29 +0500

    remove OfflineLauncher.cs and OnlineLauncher.cs

--- Tests/LauncherTests/OnlineLauncherTests.cs
+++ Tests/LauncherTests/OnlineLauncherTests.cs
@@ -1,5 +1,4 @@
 using System.Diagnostics;
-using Launcher;
 using Launcher.PublicData;
 
 namespace LauncherTests;
@@ -9,53 +8,54 @@ public class OnlineLauncherTests
     private string _temporaryGameFolder = string.Empty;
     private Versions? _availableVersions;
 
-    [SetUp]
-    public async Task Setup()
-    {
-        _temporaryGameFolder = Path.GetTempPath() + "minecraftTests\\";
-        
-        var launcher = new OnlineLauncher();
-        _availableVersions = await launcher.GetAvailableVersions(_temporaryGameFolder, default);
-    }
-
-    [Test]
-    public async Task GetAvailableVersions_True_IsNotEqualEmpty()
-    {
-        var launcher = new OnlineLauncher();
-        var availableVersions = await launcher.GetAvailableVersions(_temporaryGameFolder, default);
-        Assert.That(Versions.Empty, Is.Not.EqualTo(availableVersions));
-    }
-
-    [Test]
-    public async Task LaunchMinecraft_True_LatestRelease()
-    {
-        if (_availableVersions?.Latest == null)
-            return;
-
-        var launcher = new OnlineLauncher();
-        var launchData = new LaunchData("Steve", _temporaryGameFolder, _availableVersions.Latest);
-        var errorCode = await launcher.LaunchMinecraft(launchData, default, startedAction: FindAndCloseMinecraft);
-
-        Assert.That(errorCode, Is.EqualTo(ErrorCode.NoError));
-    }
-    
-    [Test]
-    public async Task LaunchMinecraft_True_AllRelease()
-    {
-        if (_availableVersions == null)
-            return;
-
-        var launcher = new OnlineLauncher();
-        foreach (var version in _availableVersions.Release)
-        {
-            var launchData = new LaunchData("Steve", _temporaryGameFolder, version);
-            var errorCode = await launcher.LaunchMinecraft(launchData, default, startedAction: FindAndCloseMinecraft);
-            if (errorCode != ErrorCode.NoError)
-                Assert.Fail($"Failed to run '{version.Id}', errorCode: '{errorCode}'");
-        }
-        
-        Assert.Pass($"Success to run {_availableVersions.Release.Count} versions");
-    }
+    //todo:
+    // [SetUp]
+    // public async Task Setup()
+    // {
+    //     _temporaryGameFolder = Path.GetTempPath() + "minecraftTests\\";
+    //     
+    //     var launcher = new OnlineLauncher();
+    //     _availableVersions = await launcher.GetAvailableVersions(_temporaryGameFolder, default);
+    // }
+    //
+    // [Test]
+    // public async Task GetAvailableVersions_True_IsNotEqualEmpty()
+    // {
+    //     var launcher = new OnlineLauncher();
+    //     var availableVersions = await launcher.GetAvailableVersions(_temporaryGameFolder, default);
+    //     Assert.That(Versions.Empty, Is.Not.EqualTo(availableVersions));
+    // }
+    //
+    // [Test]
+    // public async Task LaunchMinecraft_True_LatestRelease()
+    // {
+    //     if (_availableVersions?.Latest == null)
+    //         return;
+    //
+    //     var launcher = new OnlineLauncher();
+    //     var launchData = new LaunchData("Steve", _temporaryGameFolder, _availableVersions.Latest);
+    //     var errorCode = await launcher.LaunchMinecraft(launchData, default, startedAction: FindAndCloseMinecraft);
+    //
+    //     Assert.That(errorCode, Is.EqualTo(ErrorCode.NoError));
+    // }
+    //
+    // [Test]
+    // public async Task LaunchMinecraft_True_AllRelease()
+    // {
+    //     if (_availableVersions == null)
+    //         return;
+    //
+    //     var launcher = new OnlineLauncher();
+    //     foreach (var version in _availableVersions.Release)
+    //     {
+    //         var launchData = new LaunchData("Steve", _temporaryGameFolder, version);
+    //         var errorCode = await launcher.LaunchMinecraft(launchData, default, startedAction: FindAndCloseMinecraft);
+    //         if (errorCode != ErrorCode.NoError)
+    //             Assert.Fail($"Failed to run '{version.Id}', errorCode: '{errorCode}'");
+    //     }
+    //     
+    //     Assert.Pass($"Success to run {_availableVersions.Release.Count} versions");
+    // }
 
     private static async void FindAndCloseMinecraft()
     {
